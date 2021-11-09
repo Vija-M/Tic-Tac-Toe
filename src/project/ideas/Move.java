3d3b0b17ca64fedbcc5f5f3b8874129f91bb6219
Vija-M <vija.riga@gmailcom>
2021-11-09 16:42:57 +0200

    Create

---
+++ src/project/ideas/Move.java
@@ -0,0 +1,29 @@
+package project.ideas;
+
+class Move {
+    private int x;
+    private int y;
+
+    public Move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
+
